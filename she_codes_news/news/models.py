9c015b4b22eb2d0545bea00d28c9de57692d445d
kalicialopez <104682798+kalicialopez@users.noreply.github.com>
2022-12-10 12:01:39 +0800

    Part 1 Project
    Articles ordered in reverse order
    Created images feature in form by adding model and linking to html

--- she_codes_news/news/models.py
+++ she_codes_news/news/models.py
@@ -1,8 +1,14 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
 
 class NewsStory(models.Model):
     title = models.CharField(max_length=200)
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE
+    )
     author = models.CharField(max_length=200)
     pub_date = models.DateTimeField()
     content = models.TextField()
+    image_url = models.URLField(blank=True)
